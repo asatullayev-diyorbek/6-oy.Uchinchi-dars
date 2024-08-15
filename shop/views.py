a10c956280d7613202d0b0d18e3190e9a720383e
asatullayev-diyorbek <asatullayevdiyorbek3@gmail.com>
2024-08-15 18:27:26 +0500

    First commit

---
+++ shop/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+
+def index(request):
+    context = {
+        'title': 'Bosh sahifa'
+    }
+    return render(request, 'index.html')
+
